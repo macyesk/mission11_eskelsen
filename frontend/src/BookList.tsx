00de41f2ff81f20b42b236592b48e531b3d56529
macy <macyeskelsen@gmail.com>
2025-03-19 13:57:04 -0600

    types file

---
+++ frontend/src/BookList.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { Book } from './types/book';
+
+
+function BookList() {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  return <></>;
+}
+
+export default BookList;
