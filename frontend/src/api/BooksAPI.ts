f5a0b7c217c3059b8ecbe85d2a7fa11955088e4a
macy <macyeskelsen@gmail.com>
2025-04-02 13:04:05 -0600

    changing sqlite to copy if newer

---
+++ frontend/src/api/BooksAPI.ts
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+import { Book } from '../types/Book';
+
+interface FetchProjectsResponse {
+  books: Book[];
+  totalNumberBooks: number;
+}
+
+const API_URL = 'http://localhost:5121/books';
+
+export const fetchProjects = async (
+  pageSize: number,
+  pageNum: number,
+  selectedCategories: string[],
+  sortByTitle: boolean
+): Promise<FetchProjectsResponse> => {
+  sortByTitle = false;
+  try {
+    const categoryParams = selectedCategories
+      .map((cat) => `categories=${encodeURIComponent(cat)}`)
+      .join('&');
+
+    const response = await fetch(
+      `${API_URL}/allbooks?pageHowMany=${pageSize}&pageNum=${pageNum}&sortBy=${sortByTitle}${selectedCategories.length ? `&${categoryParams}` : ''}`,
+      {
+        credentials: 'include',
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch books');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    throw error;
+  }
+};
+
+export const addBook = async (newBook: Book): Promise<Book> => {
+  try {
+    const response = await fetch(`${API_URL}/AddBook`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newBook),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to add book');
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error adding book', error);
+    throw error;
+  }
+};
+
+export const updateBook = async (
+  bookId: number,
+  updatedBook: Book
+): Promise<Book> => {
+  try {
+    const response = await fetch(`${API_URL}/UpdateBook/${bookId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedBook),
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error('Erorr updating book: ', error);
+    throw error;
+  }
+};
+
+export const deleteBook = async (bookId: number): Promise<void> => {
+  try {
+    const response = await fetch(`${API_URL}/DeleteBook/${bookId}`, {
+      method: 'DELETE',
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to delete book');
+    }
+  } catch (error) {
+    console.error('Error deleting book: ', error);
+    throw error;
+  }
+};
