b0628b2ca925d45d1113f15a1d7511b4a49d0ea2
macy <macyeskelsen@gmail.com>
2025-04-02 14:48:53 -0600

    saving app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,8 +14,11 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<BooksPage />} />
-            <Route path="/books
-            " element={<BooksPage />} />
+            <Route
+              path="/books
+            "
+              element={<BooksPage />}
+            />
             <Route
               path="/addToCart/:title/:bookId/:price"
               element={<AddToCartPage />}
